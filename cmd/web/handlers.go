1153cd4b59885429c5f3fb49caf5f7757c01b7c0
“ArvidEllan” <ellanwambugu6@gmail.com>
2023-06-20 15:35:14 +0300

    updates on the templates and logic with handlers

---
+++ cmd/web/handlers.go
@@ -0,0 +1,47 @@
+package main
+
+import(
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/" {
+		http.NotFound(w,r);
+		return
+	}
+	ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "internal Server Error",500)
+		return
+	}
+	err = ts.Execute(w, nil)
+	if err != nil{
+		log.Println(err.Error())
+		http.Error(w,"Internal server error ",500 )
+	}
+	w.Write([]byte("hello from snippetbox"))
+}
+
+func showSnippet(w http.ResponseWriter , r *http.Request){
+	id,err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w,r)
+		return
+	}
+
+	fmt.Printf("%s",w, "Display a specific snippet with ID %d" , id)
+}
+
+func createSnippet(w http.ResponseWriter , r *http.Request){
+	if r.Method != "POST"{
+		w.Header().Set("Allow", "POST")
+		http.Error(w, "method Not Allowed",405)
+		return
+	}
+	w.Write([]byte("create a new snipper"))
+}
