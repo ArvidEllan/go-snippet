653783b04801a7f580629c0eff82e978f9824955
“ArvidEllan” <ellanwambugu6@gmail.com>
2023-06-21 09:25:40 +0300

    changes in handling errors

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,53 +1,53 @@
+
 package main
 
-import(
+import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
-	
 )
 
-func home(w http.ResponseWriter, r *http.Request){
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w,r);
+		app.NotFound(w)
 		return
 	}
+
 	files := []string{
 		"./ui/html/home.page.tmpl",
 		"./ui/html/base.layout.tmpl",
 		"./ui/html/footer.partial.tmpl",
 	}
+
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "internal Server Error",500)
+		app.serverError(w, err)
 		return
 	}
+
 	err = ts.Execute(w, nil)
-	if err != nil{
-		log.Println(err.Error())
-		http.Error(w,"Internal server error ",500 )
+	if err != nil {
+		app.serverError(w, err)
 	}
-	w.Write([]byte("hello from snippetbox"))
 }
 
-func showSnippet(w http.ResponseWriter , r *http.Request){
-	id,err := strconv.Atoi(r.URL.Query().Get("id"))
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w,r)
+		app.NotFound(w)
 		return
 	}
 
-	fmt.Printf("%s",w, "Display a specific snippet with ID %d" , id)
+	fmt.Fprintf(w, "Display a specific snippet with ID %d", id)
 }
 
-func createSnippet(w http.ResponseWriter , r *http.Request){
-	if r.Method != "POST"{
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
 		w.Header().Set("Allow", "POST")
-		http.Error(w, "method Not Allowed",405)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
-	w.Write([]byte("create a new snipper"))
+
+	w.Write([]byte("create a new snippet"))
 }
